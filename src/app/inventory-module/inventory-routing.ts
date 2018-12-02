017d9365df16ede9d61ac4c4503be4b7d1bd8bb8
rocky-bgta <rocky.bgta@gmail.com>
2018-12-02 17:02:35 +0600

    create product component

--- src/app/inventory-module/inventory-routing.ts
+++ src/app/inventory-module/inventory-routing.ts
@@ -1,12 +1,14 @@
 import {Routes} from '@angular/router';
 import {CategoryComponent} from "./category/category.component";
+import {ProductComponent} from "./product/product.component";
 
 
 export const inventoryRoutes: Routes = [
   {
     path: '',
     children: [
-      {path: 'category', component: CategoryComponent}
+      {path: 'category', component: CategoryComponent, pathMatch: 'full'},
+      {path: 'product', component: ProductComponent, pathMatch: 'full'}
     ]
   },
 ];
--- src/app/inventory-module/inventory.module.ts
+++ src/app/inventory-module/inventory.module.ts
@@ -5,13 +5,13 @@ import {inventoryRoutes} from "./inventory-routing";
 import {CategoryComponent} from "./category/category.component";
 import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 import {HttpClientModule} from "@angular/common/http";
-import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
 import {TranslateModule} from "@ngx-translate/core";
 import {DataTablesModule} from "angular-datatables";
 import {NgSelectModule} from "@ng-select/ng-select";
+import {ProductComponent} from './product/product.component';
 
 @NgModule({
-  declarations: [CategoryComponent],
+  declarations: [CategoryComponent, ProductComponent],
   imports: [
     [
       RouterModule.forChild(inventoryRoutes)],
