096c153065fc9098d5b481006021213018457848
Salahin Rocky <nazmus.rocky@synesisit.com.bd>
2018-12-10 14:36:22 +0600

    create base component

--- src/app/core/components/base/base.component.ts
+++ src/app/core/components/base/base.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Injector, OnInit} from '@angular/core';
+import {ToastrService} from "ngx-toastr";
+import {ICustomType} from "../../interface/ICustomType";
+import * as lodash from 'lodash';
 
 @Component({
   selector: 'app-base',
@@ -7,7 +10,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BaseComponent implements OnInit {
 
-  constructor() { }
+  public customObject: ICustomType;
+  public lodash;
+  public toasterService: ToastrService;
+  private injector: Injector;
+
+  constructor() {
+    this.toasterService = this.injector.get(ToastrService);
+    this.customObject = {};
+    this.lodash = lodash;
+  }
 
   ngOnInit() {
   }
