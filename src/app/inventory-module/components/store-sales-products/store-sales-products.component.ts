345e2b7f75713865035e49b2ff2c2e24f6ab05c8
Salahin <rocky.bgta@gmail.com>
2018-12-31 21:56:40 +0600

    remove un-used code

--- src/app/inventory-module/components/store-sales-products/store-sales-products.component.ts
+++ src/app/inventory-module/components/store-sales-products/store-sales-products.component.ts
@@ -1,15 +1,12 @@
 import {AfterViewInit, Component, ElementRef, OnDestroy, OnInit, ViewChild} from '@angular/core';
 import {FormBuilder, FormGroup, NgForm, Validators} from "@angular/forms";
 import {ToastrService} from "ngx-toastr";
-import {VendorService} from "../../service/vendor.service";
 import {NgxSmartModalService} from "ngx-smart-modal";
 import {ResponseMessage} from "../../../core/model/response-message";
 import {Util} from "../../../core/Util";
 import {RequestMessage} from "../../../core/model/request-message";
 import {HttpErrorResponse} from "@angular/common/http";
-import {VendorModel} from "../../model/vendor-model";
 import {StoreModel} from "../../model/store-model";
-import {ProductModel} from "../../model/product-model";
 import {StoreService} from "../../service/store.service";
 import * as _ from 'lodash';
 import {HttpStatusCode} from "../../../core/constants/HttpStatusCode";
@@ -142,45 +139,11 @@ export class StoreSalesProductsComponent implements OnInit,  AfterViewInit, OnDe
       this.dtTrigger.next();
     });
   }
-/*
 
-  public onClickAddProduct(){
-    //set this value for validation purpose only
-    this.productAdded=true;
-    //==========================================
-
-    if(this.storeSelected && this.customerSelected) {
-      this.barcodeRef.nativeElement.disabled = false;
-      this.barcodeRef.nativeElement.focus();
-    }
-
-
-    //First check if any invalid entry exist
-    if(this.entryForm.invalid){
-      this.toastr.error("Please correct added product data first", this.pageTitle);
-      return;
-    }else {
-      if (!this.entryForm.invalid) {
-        this.productAdded=false;
-        Util.logConsole(this.storeInProductViewModelList,"List");
-        this.addProductToList();
-        return;
-      }
-    }
-  }
-*/
 
   public onClickClear(){
-    this.storeInProductViewModel = new StoreInProductViewModel();
-    this.storeInProductViewModel.entryDate = new Date();
-    this.storeSelected=false;
-    this.customerSelected=false;
-    this.productAdded=false;
+    this.resetPage();
   }
-
- /* public onClickClearAllAddedProduct(){
-    this.storeInProductViewModelList = new Array<StoreInProductViewModel>();
-  }*/
 
   public onClearStore(){
     this.storeSelected=false;
@@ -191,7 +154,6 @@ export class StoreSalesProductsComponent implements OnInit,  AfterViewInit, OnDe
   }
 
   public onChangeStore(event,storeId:string){
-
     this.storeSalesProductViewModel.storeId=null;
     this.storeSalesProductViewModel.storeId = storeId;
     this.storeId = storeId;
@@ -199,7 +161,6 @@ export class StoreSalesProductsComponent implements OnInit,  AfterViewInit, OnDe
       this.storeSelected = true;
       this.setFocusOnBarcodeInputTextBox();
     }
-    //this.rerender();
   }
 
   public onChangeBarcode(barcode:string, event){
@@ -209,7 +170,6 @@ export class StoreSalesProductsComponent implements OnInit,  AfterViewInit, OnDe
     this.rerender();
     event.target.select();
     event.target.value="";
-
   }
 
   public onChangeSerialNo(serialNo:string, event){
@@ -219,7 +179,6 @@ export class StoreSalesProductsComponent implements OnInit,  AfterViewInit, OnDe
     this.rerender();
 
   }
-
 
   public onChangeCustomer(event, customerId:string){
     this.storeSalesProductViewModel.customerId=null;
@@ -234,14 +193,6 @@ export class StoreSalesProductsComponent implements OnInit,  AfterViewInit, OnDe
     this.customerSelected=false;
   }
 
-  public onChangeProduct(event){
-
-  }
-
-  public onClearProduct(){
-
-  }
-
   public onChangeSaleMethod(event){
 
   }
@@ -249,7 +200,6 @@ export class StoreSalesProductsComponent implements OnInit,  AfterViewInit, OnDe
   public onClearSaleMethod(){
 
   }
-
 
   public onFocusOutSalesPriceRowEvent(index:number, salesPrice:number){
     let isAllowedSalePrice:boolean;
@@ -285,17 +235,25 @@ export class StoreSalesProductsComponent implements OnInit,  AfterViewInit, OnDe
       this.toastr.info("Please correct entered sales products value");
     }
     return;
-
   }
 
   public onClickReset(){
-
+      this.resetPage();
   }
 
   public onFocusOutPaidAmount(paidAmount:number){
     this.setDueAmount(paidAmount);
   }
 
+  private resetPage(){
+    this.storeSalesProductViewModel = new StoreSalesProductViewModel();
+    this.storeSelected=false;
+    this.customerSelected=false;
+    this.availableSalesProductViewModelList=null;
+    this.storeSalesProductViewModel.storeId=null;
+    this.storeSalesProductViewModel.customerId=null;
+    this.storeSalesProductViewModel.salesMethod=null;
+  }
 
   private setRowWiseTotalPrice(index:number){
     let salesPrice: number;
@@ -376,7 +334,6 @@ export class StoreSalesProductsComponent implements OnInit,  AfterViewInit, OnDe
     )
   }
 
-
   private saveStoreSalesProduct(){
     this.storeSalesProductViewModel.salesProductViewModelList = this.availableSalesProductViewModelList;
 
@@ -393,8 +350,7 @@ export class StoreSalesProductsComponent implements OnInit,  AfterViewInit, OnDe
           this.toastr.error(responseMessage.message,this.pageTitle);
         }else if(responseMessage.httpStatus==HttpStatusCode.CREATED){
           this.toastr.success( responseMessage.message,this.pageTitle);
-          //this.vendorModel = <VendorModel> responseMessage.data;
-          //this.getVendorList(this.dataTablesCallBackParameters, this.dataTableCallbackFunction);
+          this.resetPage();
           return;
         }else {
           this.toastr.error(responseMessage.message,this.pageTitle);
@@ -484,126 +440,6 @@ export class StoreSalesProductsComponent implements OnInit,  AfterViewInit, OnDe
       }
 
     )
-  }
-
-  private async getProductByBarcode(barcode:string):Promise<ProductModel>{
-    let productModel: ProductModel = null;
-    await this.productService.getByBarcodeAsync(barcode.trim()).then
-    (
-      (responseMessage:ResponseMessage)=>
-      {
-        if(responseMessage.httpStatus==HttpStatusCode.FOUND){
-          productModel = <ProductModel>responseMessage.data;
-          //============== code need re-factor need to do promise base code ======================
-          // this.storeInProductViewModel.productName = productModel.name;
-          // this.storeInProductViewModel.productId = productModel.id;
-          // this.storeInProductViewModel.price = productModel.price;
-          // this.setTotalPrice();
-          // this.addProductToList();
-          //======================================================================================
-
-          //Util.logConsole(productModel,"Product Model from subscribe");
-          return productModel;
-        }else if(responseMessage.httpStatus==HttpStatusCode.NOT_FOUND) {
-          this.toastr.error(responseMessage.message,this.pageTitle);
-          return;
-        }else {
-          Util.logConsole(responseMessage);
-          return;
-        }
-      }
-    ).catch(
-      (httpErrorResponse: HttpErrorResponse) =>
-      {
-        if (httpErrorResponse.error instanceof ErrorEvent) {
-          Util.logConsole(httpErrorResponse,"Client-side error occurred.");
-        } else {
-          this.toastr.error('There is a problem with the service. We are notified and working on it');
-          this.toastr.info("Please reload this page");
-          Util.logConsole(httpErrorResponse,"Server Side error occurred" );
-        }
-        //request.unsubscribe();
-        return;
-      }
-    );
-    return productModel;
-  }
-
-  private async getProductListByBarcode(barcode:string):Promise<ProductModel>{
-    let productModel: ProductModel = null;
-    await this.productService.getByBarcodeAsync(barcode.trim()).then
-    (
-      (responseMessage:ResponseMessage)=>
-      {
-        if(responseMessage.httpStatus==HttpStatusCode.FOUND){
-          productModel = <ProductModel>responseMessage.data;
-          //============== code need re-factor need to do promise base code ======================
-          // this.storeInProductViewModel.productName = productModel.name;
-          // this.storeInProductViewModel.productId = productModel.id;
-          // this.storeInProductViewModel.price = productModel.price;
-          // this.setTotalPrice();
-          // this.addProductToList();
-          //======================================================================================
-
-          //Util.logConsole(productModel,"Product Model from subscribe");
-          return productModel;
-        }else if(responseMessage.httpStatus==HttpStatusCode.NOT_FOUND) {
-          this.toastr.error(responseMessage.message,this.pageTitle);
-          return;
-        }else {
-          Util.logConsole(responseMessage);
-          return;
-        }
-      }
-    ).catch(
-      (httpErrorResponse: HttpErrorResponse) =>
-      {
-        if (httpErrorResponse.error instanceof ErrorEvent) {
-          Util.logConsole(httpErrorResponse,"Client-side error occurred.");
-        } else {
-          this.toastr.error('There is a problem with the service. We are notified and working on it');
-          this.toastr.info("Please reload this page");
-          Util.logConsole(httpErrorResponse,"Server Side error occurred" );
-        }
-        //request.unsubscribe();
-        return;
-      }
-    );
-    return productModel;
-  }
-
-  private async getProductBySerialNo(serialNo:string):Promise<ProductModel>{
-    let productModel: ProductModel = null;
-    await this.productService.getByBarcodeAsync(serialNo.trim()).then
-    (
-      (responseMessage:ResponseMessage)=>
-      {
-        if(responseMessage.httpStatus==HttpStatusCode.FOUND){
-          productModel = <ProductModel>responseMessage.data;
-          return productModel;
-        }else if(responseMessage.httpStatus==HttpStatusCode.NOT_FOUND) {
-          this.toastr.error(responseMessage.message,this.pageTitle);
-          return;
-        }else {
-          Util.logConsole(responseMessage);
-          return;
-        }
-      }
-    ).catch(
-      (httpErrorResponse: HttpErrorResponse) =>
-      {
-        if (httpErrorResponse.error instanceof ErrorEvent) {
-          Util.logConsole(httpErrorResponse,"Client-side error occurred.");
-        } else {
-          this.toastr.error('There is a problem with the service. We are notified and working on it');
-          this.toastr.info("Please reload this page");
-          Util.logConsole(httpErrorResponse,"Server Side error occurred" );
-        }
-        //request.unsubscribe();
-        return;
-      }
-    );
-    return productModel;
   }
 
   private getAvailableStoreInProductListByStoreIdOrBarcodeOrSerialNo(dataTablesParameters: DataTableRequest, callback: any, storeId?: string, barcode?:string, serialNo?:string){
