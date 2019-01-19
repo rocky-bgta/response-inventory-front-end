abaa00f08fb12a04c6e446461993e3789bf72d4b
Salahin <rocky.bgta@gmail.com>
2019-01-19 10:41:20 +0600

    fixed issue of product sales

--- src/app/inventory-module/components/product-sales/product-sales.component.ts
+++ src/app/inventory-module/components/product-sales/product-sales.component.ts
@@ -210,6 +210,9 @@ export class ProductSalesComponent implements OnInit {
     if (salesQty > availableQty) {
       this.selectedProductListForSales[index].salesQty = availableQty;
     }
+    if(salesQty==""){
+      this.selectedProductListForSales[index].salesQty = 1;
+    }
     this.setRowWiseDiscountSalesPrice(index);
     this.setRowWiseTotalPrice(index);
   }
